772ff7a8eab5fe98cba88eda00f5bc54447ec061
Avinash <avinash@heresy.in>
2020-12-31 16:36:30 +0530

    first commit

---
+++ src/demos/Construction.js
@@ -0,0 +1,136 @@
+import React from "react";
+import tw from "twin.macro";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import Hero from "components/hero/TwoColumnWithVideo.js";
+import Features from "components/features/ThreeColSimple.js";
+import MainFeature from "components/features/TwoColWithButton.js";
+import MainFeature2 from "components/features/TwoColSingleFeatureWithStats2.js";
+import TabGrid from "components/cards/TabCardGrid.js";
+import Testimonial from "components/testimonials/ThreeColumnWithProfileImage.js";
+import DownloadApp from "components/cta/DownloadApp.js";
+import Footer from "components/footers/FiveColumnWithInputForm.js";
+import SliderCard from "components/cards/ThreeColSlider.js";
+import   TrendingCard from "components/cards/PortfolioTwoCardsWithImage";
+
+
+import chefIconImageSrc from "images/chef-icon.svg";
+import celebrationIconImageSrc from "images/celebration-icon.svg";
+import shopIconImageSrc from "images/shop-icon.svg";
+
+export default () => {
+  const Subheading = tw.span`tracking-wider text-sm font-medium`;
+  const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
+  const HighlightedTextInverse = tw.span`bg-gray-100 text-primary-500 px-4 transform -skew-x-12 inline-block`;
+  const Description = tw.span`inline-block mt-8`;
+  const imageCss = tw`rounded-4xl`;
+  return (
+    <AnimationRevealPage>
+      <Hero
+        heading={<>Affordable & Great Quality <HighlightedText>Houses For You.</HighlightedText></>}
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        imageSrc="https://specials-images.forbesimg.com/imageserve/5c0077cc31358e5b43383ffc/960x0.jpg?fit=scale"
+        imageCss={imageCss}
+        imageDecoratorBlob={true}
+        primaryButtonText="Contact Now"
+        watchVideoButtonText="Meet The Builders"
+      />
+      <MainFeature
+        subheading={<Subheading>Established Since 2014</Subheading>}
+        heading={
+          <>
+            We've been serving for
+            <wbr /> <HighlightedText>over 5 years.</HighlightedText>
+          </>
+        }
+        description={
+          <Description>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+            <br />
+            <br />
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </Description>
+        }
+        buttonRounded={false}
+        textOnLeft={false}
+        primaryButtonText="Latest Offers"
+        imageSrc={
+"https://i2.wp.com/theconstructor.org/wp-content/uploads/2012/10/construction-company.jpg?fit=3372%2C1897&ssl=1"        }
+        imageCss={imageCss}
+        imageDecoratorBlob={true}
+        imageDecoratorBlobCss={tw`left-1/2 -translate-x-1/2 md:w-32 md:h-32 opacity-25`}
+      />
+     
+       <TrendingCard 
+         subheading ="Current Project"
+       
+       />
+      <Features
+        heading={
+          <>
+            Amazing <HighlightedText>Service.</HighlightedText>
+          </>
+        }
+        cards={[
+          {
+            imageSrc: shopIconImageSrc,
+            title: "Trending Location",
+            description: "Lorem ipsum donor amet siti ceali placeholder text",
+            url: "https://google.com"
+          },
+          {
+            imageSrc: chefIconImageSrc,
+            title: "Professional Builders",
+            description: "Lorem ipsum donor amet siti ceali placeholder text",
+            url: "https://timerse.com"
+          },
+          {
+            imageSrc: celebrationIconImageSrc,
+            title: "Great Quality",
+            description: "Lorem ipsum donor amet siti ceali placeholder text",
+            url: "https://reddit.com"
+          }
+        ]}
+
+        imageContainerCss={tw`p-2!`}
+        imageCss={tw`w-20! h-20!`}
+      />
+      <MainFeature2
+        subheading={<Subheading>A Reputed Brand</Subheading>}
+        heading={<>Why <HighlightedText>Choose Us ?</HighlightedText></>}
+        statistics={[
+          {
+            key: "Orders",
+            value: "94000+",
+          },
+          {
+            key: "Customers",
+            value: "11000+"
+          },
+          {
+            key: "Chefs",
+            value: "1500+"
+          }
+        ]}
+        primaryButtonText="Order Now"
+        primaryButtonUrl="https://order.now.com"
+        imageInsideDiv={false}
+        imageSrc="https://specials-images.forbesimg.com/imageserve/5c0077cc31358e5b43383ffc/960x0.jpg?fit=scale"
+        imageCss={Object.assign(tw`bg-cover`, imageCss)}
+        imageContainerCss={tw`md:w-1/2 h-auto`}
+        imageDecoratorBlob={true}
+        imageDecoratorBlobCss={tw`left-1/2 md:w-32 md:h-32 -translate-x-1/2 opacity-25`}
+        textOnLeft={true}
+      />
+      <Testimonial
+        subheading=""
+        heading={<>Customers <HighlightedText>Love Us.</HighlightedText></>}
+      />
+      <DownloadApp
+        text={<>People around you are ordering delicious meals using the <HighlightedTextInverse>Treact App.</HighlightedTextInverse></>}
+      />
+      <Footer />
+    </AnimationRevealPage>
+  );
+}
